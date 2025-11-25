605279f050b1b2ca1f7385065c06628665340d55
Hans Vador <hansvador7@gmail.com>
2025-11-25 01:17:13 +0000

    added smooth movement and bounded to joystick

--- RPI/Controller.py
+++ RPI/Controller.py
@@ -9,6 +9,7 @@ BAUD_RATE = 115200
 COXA = 43
 FEMUR = 60
 TIBIA = 110
+SPEED = 0.2
 
 def clamp(value, min_value, max_value):
     return max(min_value, min(value, max_value))
@@ -50,10 +51,17 @@ class MyController(Controller):
     def __init__(self, serial_port=SERIAL_PORT, **kwargs):
         self.x = 0
         self.y = 120
-        self.z = 60
+        self.z = 0
         self.ik = IKEngine()
         super().__init__(**kwargs)
 
+        #Initial State
+        self.state = "idle"
+        self.xMultiplier = 0
+        self.yMultiplier = 0
+        self.zMultiplier = 0
+        self.speed = 0.2
+        self.moved = False
         # Serial connection to Servo2040
         self.serial_port = serial_port
         self.serial_conn = serial.Serial(serial_port, BAUD_RATE, timeout=0.1)
@@ -101,8 +109,25 @@ class MyController(Controller):
         while True:
             with self._command_lock:
                 target = self.desired_command  # whatever YOU compute
-            if target:
-                self.send_command(target)
+            
+            if self.state == "idle":
+                 if target:
+                      self.send_command(target)
+            if self.xMultiplier != 0:
+                 self.x += self.xMultiplier * self.speed
+                 self.moved = True
+            if self.yMultiplier != 0:
+                 self.y += self.yMultiplier * self.speed
+                 self.moved = True
+            if self.zMultiplier != 0:
+                 self.z += self.zMultiplier * self.speed
+                 self.moved = True
+            if self.moved:
+                 self._update_ik()
+                 self.send_command(target)
+                 self.moved = False
+                 
+            
             time.sleep(0.02)  # 50 Hz update for smooth robotics
 
     # -------------------------------
@@ -152,13 +177,20 @@ class MyController(Controller):
                 self._update_ik()
 
     # Empty methods for all other controls to silence them
-    def on_x_press(self): pass
+    def on_x_press(self): 
+         self.speed += 0.2
     def on_x_release(self): pass
-    def on_triangle_press(self): pass
+    def on_triangle_press(self): 
+         self.y = 45
+         self._update_ik()
     def on_triangle_release(self): pass
-    def on_circle_press(self): pass
+    def on_circle_press(self): 
+         self.y += 5
+         self._update_ik()
     def on_circle_release(self): pass
-    def on_square_press(self): pass
+    def on_square_press(self): 
+         self.y -= 5
+         self._update_ik()
     def on_square_release(self): pass
     def on_L1_press(self): pass
     def on_L1_release(self): pass
@@ -174,10 +206,30 @@ class MyController(Controller):
     def on_L3_y_at_rest(self): pass
     def on_L3_press(self): pass
     def on_L3_release(self): pass
-    def on_L3_up(self, value): pass
-    def on_L3_down(self, value): pass
-    def on_L3_left(self, value): pass
-    def on_L3_right(self, value): pass
+    def on_L3_up(self, value): 
+            speed = value/-10000
+            if speed > 1:
+                self.zMultiplier = speed
+            else:
+                 self.zMultiplier = 0
+    def on_L3_down(self, value): 
+            speed = value/-10000
+            if speed * -1 > 1:
+                self.zMultiplier = speed
+            else:
+                 self.zMultiplier = 0
+    def on_L3_left(self, value): 
+            speed = value/10000
+            if speed * -1 > 1:
+                self.xMultiplier = speed
+            else:
+                 self.xMultiplier = 0
+    def on_L3_right(self, value): 
+            speed = value/10000
+            if speed > 1:
+                self.xMultiplier = speed
+            else:
+                 self.xMultiplier = 0
     def on_R3_up(self, value): pass
     def on_R3_down(self, value): pass
     def on_R3_left(self, value): pass
